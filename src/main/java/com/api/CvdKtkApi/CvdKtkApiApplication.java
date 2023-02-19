ea0cc86a0a77d8cc59e9c751556cce94fb0e5d13
Chethan S <cchethans14@gmail.com>
2023-02-19 15:18:40 +0530

    2022-02-19

--- src/main/java/com/api/CvdKtkApi/CvdKtkApiApplication.java
+++ src/main/java/com/api/CvdKtkApi/CvdKtkApiApplication.java
@@ -4,13 +4,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Objects;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.api.CvdKtkApi.Constants.Constants;
@@ -18,34 +18,26 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
-
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 @EnableScheduling
 public class CvdKtkApiApplication {
 
-	public static void main(String[] args) throws FileNotFoundException {
-		
-		ClassLoader classLoader = CvdKtkApiApplication.class.getClassLoader();
+	public static void main(String[] args) throws IOException {
 
-		File file = new File(classLoader.getResource("serviceKey.json").getPath());
+		ClassPathResource resource = new ClassPathResource("serviceKey.json");
+		InputStream inputStream = resource.getInputStream();
 
 		try {
 			FirebaseOptions options;
-			FileInputStream serviceAccount =
-					  new FileInputStream(file.getAbsolutePath());
-			System.out.println(serviceAccount);
-		
-//			options = FirebaseOptions.builder()
-//				    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-//				    .setDatabaseUrl("https://corona-karnataka-2020.firebaseio.com")
-//				    .build();
-//			FirebaseApp.initializeApp(options);
-					
+
+			options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(inputStream))
+					.setDatabaseUrl("https://corona-karnataka-2020.firebaseio.com").build();
+			FirebaseApp.initializeApp(options);
+
 			SpringApplication.run(CvdKtkApiApplication.class, args);
-			
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-		} catch (Exception e) {
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
